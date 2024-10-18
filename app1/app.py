62d246130524744c542cc9f1d82fc134886e8553
Sagar Gidwani <86357062+SagarGidwani@users.noreply.github.com>
2024-10-19 04:10:19 +0530

    Update app.py

--- app1/app.py
+++ app1/app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create a Flask application
 app = Flask(__name__)
 
 # Define a route for the root URL
 @app.route('/')
-def hello_world():
-    return 'Hello, World App 1!'
+def home():
+    return render_template('index.html')
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
-    app.run(debug=True, port=8080,host='0.0.0.0')
+    app.run(debug=True, port=8080, host='0.0.0.0')
